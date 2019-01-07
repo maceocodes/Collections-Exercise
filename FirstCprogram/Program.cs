fd914a85d6c06870fd123f858811134a958e20ba
Maceo <mlewisjr89@gmail.com>
2019-01-07 11:48:17 -0500

    test

---
+++ FirstCprogram/Program.cs
@@ -0,0 +1,13 @@
+class Program
+{
+    static void Main()
+    {
+        //Prompt the user for minutes exercised
+        System.Console.Write("How many minutes did you exercise today");
+
+        //Add mins exercied to total
+        //Dusokay total mins exercies to the screen
+        //repeat until the user quits
+        
+    }
+}
