57356227d9e3abf1641ecef97986e3a7345fd7de
Maceo <mlewisjr89@gmail.com>
2019-01-09 14:57:24 -0500

    First C# application complete

--- FirstCprogram/Program.cs
+++ FirstCprogram/Program.cs
@@ -1,13 +1,36 @@
-class Program
-{
-    static void Main()
-    {
-        //Prompt the user for minutes exercised
-        System.Console.Write("How many minutes did you exercise today");
+﻿using System;
 
-        //Add mins exercied to total
-        //Dusokay total mins exercies to the screen
-        //repeat until the user quits
-        
+namespace FirstCprogram
+{
+    class Program
+    {
+        static void Main()
+        {
+            int runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                //Prompt the user for minutes exercised
+                Console.Write( "How many minutes did you exercise or type \"quit\" to exit: " );
+
+                //Collect user input and save to variable
+                string entry = Console.ReadLine();
+                
+                if (entry == "quit") 
+                {
+                    keepGoing = false;
+                }
+                //Parse user input for integer and add to the running total
+                int minutes = int.Parse(entry);
+                runningTotal = runningTotal + minutes;
+
+                //Display total mins exercies to the screen
+                Console.WriteLine("You've exercised " + runningTotal + " minutes");
+                //repeat until the user quits
+            }
+
+            Console.WriteLine("Goodbye");
+        }
     }
 }
